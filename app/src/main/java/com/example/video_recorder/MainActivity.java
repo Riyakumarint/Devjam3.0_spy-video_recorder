faf842ebc24be573a2d4f41195b437efe13c2fda
Riya kumari <riyakumari@Riyas-MacBook-Air.local>
2020-05-11 19:43:21 +0530

    Devjam3.0

---
+++ app/src/main/java/com/example/video_recorder/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.video_recorder;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.record).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 3600);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == RESULT_OK && requestCode == 1) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            VideoView videoView = new VideoView(this);
+            videoView.setVideoURI(data.getData());
+            videoView.start();
+            builder.setView(videoView).show();
+        }
+
+    }
+}
